b53ce0665e96b8d85646904c3ed04649e50c0887
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-07 15:40:41 -0700

    .reduce blows my mind

--- higher-order.js
+++ higher-order.js
@@ -134,8 +134,7 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal = purchases.reduce(
-  function()
+let bobsTotal = purchases.reduce(function(acc, curr){
+    return acc.bob.price + curr.bob.price
+  }
 )
-
-
